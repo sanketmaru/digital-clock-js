d6ce837b68916f2b87c2dd210c8c578c784912d9
sanketmaru <sanketmaru@gmail.com>
2015-05-24 10:25:57 +0530

    working version of digital clock

--- app.js
+++ app.js
@@ -1,39 +1,54 @@
-function Clock(){
-	this.currentSeconds = 0;
-	this.currentMinutes = 0;
-	this.currentHours = 0;
+function Clock(currentHours, currentMinutes, currentSeconds){
+	this.currentSeconds = currentSeconds || 0;
+	this.currentMinutes = currentMinutes || 0;
+	this.currentHours = currentHours || 0;
+	this.maxSeconds = 59;
+	this.maxMinutes = 59;
+	this.maxHours = 11;
 };
 
 
 Clock.prototype.start = function(){
-	return this.incrementSeconds();
+	this.incrementSeconds();
 };
 
 Clock.prototype.pause = function(){
-
+	this.clearInterval(this.clockTimer);
 };
 
 Clock.prototype.incrementSeconds = function(){
 	var self = this;
 	this.clockTimer = setInterval(function(){
-		++self.currentSeconds;
-		console.log("In Increment Seconds" + self.currentSeconds);
-	}, 1000);
 
-	return {
-		seconds: this.currentSeconds
-	}
+		if(this.currentSeconds === this.maxSeconds){
+
+			if(this.currentMinutes === this.maxMinutes){
+
+				if(this.currentHours === this.maxHours){
+					this.currentHours = 0;
+				}
+
+				++this.maxHours;
+
+			}
+			this.currentSeconds = 0;
+			++this.currentMinutes;
+		}
+		++this.currentSeconds;
+
+	}.bind(this), 1000);
+
 };
 
 Clock.prototype.getTime = function(){
-	return this.currentSeconds;
+	return this.currentHours + " " + this.currentMinutes + " " + this.currentSeconds;
 };
 
 
 (function(){
-	var c = new Clock();
+	var c = new Clock(0, 0, 50);
 	var displayTime = c.start();
 	setInterval(function(){
 		console.log(c.getTime());
-	}, 0);
+	}, 1000);
 })();
